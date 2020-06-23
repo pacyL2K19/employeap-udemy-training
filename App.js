ca6b1dfb1d04ff5679174010529d7fffb4b75208
Pacifique Linjanja <pacilinja1@icloud.com>
2020-06-23 14:29:57 +0200

    First section of the course, undestanding basic code

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import home from './screens/Home'
+import Home from './screens/Home';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Home />
     </View>
   );
 }
---
+++ screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { Card } from 'react-native-paper'
+
+const Home = () => {
+    return (
+        <Card>
+            <Text>Hello from home</Text>
+        </Card>
+    )
+}
+
+export default Home
