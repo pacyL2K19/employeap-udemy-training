c89cf860e672b6e8e978313bbdb0731ad326b8f2
Pacifique Linjanja <pacilinja1@icloud.com>
2020-06-25 12:49:34 +0200

    Minor update

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View , Image} from 'react-native'
 import { Card } from 'react-native-paper'
 
 const Home = () => {
     return (
         <Card style = { styles.myCard }>
-            <Text>Hello from home</Text>
+            <View style = { styles.viewCard}> 
+                <Image 
+                    style = {{ height : 60, width : 60, borderRadius : 30}}
+                    source = {{ uri : "https://images.unsplash.com/photo-1587890271791-6fc0d1fe7537?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=764&q=80"}}
+                />
+                <Text>Hello from home</Text>
+            </View>
         </Card>
     )
 }
@@ -13,7 +19,11 @@ const Home = () => {
 const styles = StyleSheet.create({
     myCard : {
         margin : 5,
-        padding : 5
+        padding : 5,
+        flexDirection : 'row'
+    },
+   viewCard : {
+        flexDirection : 'row'
     }
 })
 
