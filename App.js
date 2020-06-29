64f6811c0995737f6926277c06e6154a2f987a3e
Pacifique Linjanja <pacilinja1@icloud.com>
2020-06-29 14:54:27 +0200

    minor update

--- App.js
+++ App.js
@@ -1,21 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import home from './screens/Home'
 import Home from './screens/Home';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    
       <Home />
-    </View>
+    
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- screens/Home.js
+++ screens/Home.js
@@ -3,6 +3,18 @@ import { StyleSheet, Text, View , Image} from 'react-native'
 import { Card } from 'react-native-paper'
 
 const Home = () => {
+    const data = [
+        {id : 1, name : 'Pacifique', position : 'Team manager'},
+        {id : 2, name : 'Cedigola', position : 'ML expert'},
+        {id : 3, name : 'Pacifique', position : 'Backend dev'},
+        {id : 4, name : 'Pacifique', position : 'Frontend dev'},
+        {id : 5, name : 'Pacifique', position : 'Devopps'},
+        {id : 6, name : 'Pacifique', position : 'Hiring manager'},
+        {id : 7, name : 'Pacifique', position : 'SEO'},
+        {id : 8, name : 'Pacifique', position : 'Teacher'},
+        {id : 9, name : 'Pacifique', position : 'iOT learner'}
+    ]
+    const 
     return (
         <Card style = { styles.myCard }>
             <View style = { styles.viewCard}> 
@@ -18,12 +30,12 @@ const Home = () => {
 
 const styles = StyleSheet.create({
     myCard : {
-        margin : 5,
-        padding : 5,
-        flexDirection : 'row'
+        marginTop : 50,
+        margin : 10
     },
-   viewCard : {
-        flexDirection : 'row'
+    viewCard : {
+        flexDirection : 'row',
+        padding: 6,
     }
 })
 
