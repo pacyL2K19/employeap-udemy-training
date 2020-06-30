42a02c97d2e3fff5087badfbdb0944b8f9986442
Pacifique Linjanja <pacilinja1@icloud.com>
2020-06-30 13:47:39 +0200

    Minor update

--- App.js
+++ App.js
@@ -1,24 +1,22 @@
 import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import CreateEmployee from './screens/CreateEmployee'
 import Home from './screens/Home';
-import { View, StyleSheet } from 'react-native';
-import CreateEmployee from './screens/CreateEmployee';
-// import Constants from 'expo-constants'
+import Constants from 'expo-constants'
 
 export default function App() {
   return (
-
-    <View style = { styles.container }> 
+    <View style={styles.container}>
       {/* <Home /> */}
       <CreateEmployee />
     </View>
-    
   );
 }
 
 const styles = StyleSheet.create({
-  container : {
-    flex : 1,
-    backgroundColor : '#e0e0e0',
-    marginTop : 20
-  }
-})
+  container: {
+    flex: 1,
+    backgroundColor : '#e0d0e0',
+    marginTop : Constants.statusBarHeight
+  },
+});
