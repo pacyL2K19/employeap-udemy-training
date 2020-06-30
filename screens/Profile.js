7d8302265bd349eb704fdb5f74d531ff9852d971
Pacifique Linjanja <pacilinja1@icloud.com>
2020-07-01 00:43:45 +0200

    Section 3 5

--- screens/Profile.js
+++ screens/Profile.js
@@ -6,12 +6,30 @@ import {
 
 } from 'react-native'
 
+import { LinearGradient } from 'expo-linear-gradient'
+import { Image } from 'react-native-paper/lib/typescript/src/components/Avatar/Avatar'
+
 const Profile = () => {
     return (
-        <View>
-
+        <View style = {styles.root}>
+            <LinearGradient 
+                colors = {['#0033ff', '#6bc1ff']}
+                style = {{ height : '20%'}}
+            />
+            <View style = {{ alignItems :'center'}}>
+                <Image
+                    style = {{width : 100, height : 100, borderRadius : 50, marginTop : -50}}
+                    source = {{uri :'https://unsplash.com/photos/y60Pux2HYk4' }}
+                />
+            </View>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    root : {
+        flex : 1,
+    }
+})
 
 export default Profile 
