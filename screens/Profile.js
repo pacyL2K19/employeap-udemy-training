e1f80d1cbc4b565090e533910157bf10cfd93893
Pacifique Linjanja <pacilinja1@icloud.com>
2020-07-01 01:14:58 +0200

    Section 3 5 designing profile file

--- screens/Profile.js
+++ screens/Profile.js
@@ -3,11 +3,12 @@ import {
     Text,
     View,
     StyleSheet,
-
+    Image
 } from 'react-native'
 
 import { LinearGradient } from 'expo-linear-gradient'
-import { Image } from 'react-native-paper/lib/typescript/src/components/Avatar/Avatar'
+import { Title, Card, Button } from 'react-native-paper'
+import { MaterialIcons, Entypo } from '@expo/vector-icons'
 
 const Profile = () => {
     return (
@@ -22,13 +23,71 @@ const Profile = () => {
                     source = {{uri :'https://unsplash.com/photos/y60Pux2HYk4' }}
                 />
             </View>
+            <View style = {{ alignItems : 'center', margin : 15}}>
+                <Title>
+                    Pacifique LINJANJA
+                </Title>
+                <Text style = {{ fontSize : 15 }}> CEO </Text>
+            </View>
+            <Card>
+                <View>
+                    <MaterialIcons name = 'email' size = {32} color = '#006aff' />
+                    <Text style = { styles.myText }> paci@linja.com </Text>
+                </View>
+            </Card>
+            <Card>
+                <View>
+                    <Entypo name = 'phone' size = {32} color = '#006aff' />
+                    <Text style = { styles.myText }> 090909939</Text>
+                </View>
+            </Card>
+            <Card>
+                <View>
+                    <MaterialIcons name = 'attach-money' size = {32} color = '#006aff' />
+                    <Text style = { styles.myText }> 100K$ </Text>
+                </View>
+            </Card>
+            <View style={{ flexDirection : 'row', justifyContent: 'space-around', padding : 10}}> 
+                <Button 
+                    icon = 'account-edit'
+                    mode = 'contained' 
+                    theme = {theme}
+                    onPress = {() => console.log('Pressed')}
+                >
+                    Edit
+                </Button>
+                <Button 
+                    icon = 'delete'
+                    mode = 'contained' 
+                    theme = {theme}
+                    onPress = {() => console.log('Pressed')}
+                >
+                    Fire employee
+                </Button>
+            </View>
         </View>
     )
+}
+
+const theme = {
+    colors : {
+        primary : '#006aff'
+    }
 }
 
 const styles = StyleSheet.create({
     root : {
         flex : 1,
+    },
+    myCard : {
+        margin : 3
+    },
+    cardContent : {
+        flexDirection : 'row',
+        padding : 8
+    },
+    myText : {
+        fontSize : 18
     }
 })
 
