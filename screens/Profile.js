a69455895bbe92cd557f99a979ffac70a81576e0
Pacifique Linjanja <pacilinja1@icloud.com>
2020-07-01 00:23:28 +0200

    Preparing profile file on Section 3 5

---
+++ screens/Profile.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {
+    Text,
+    View,
+    StyleSheet,
+
+} from 'react-native'
+
+const Profile = () => {
+    return (
+        <View>
+
+        </View>
+    )
+}
+
+export default Profile 
