6666e844ce0972f8e5ad8d8c181d480227e33355
Pacifique Linjanja <pacilinja1@icloud.com>
2020-07-06 01:38:40 +0200

    setting up app.js file and first test of the server file

--- server/app.js
+++ server/app.js
@@ -1,8 +1,22 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
+import Employee from './Employee'
 const app = express ()
 const mongoUrl = 'mongo "mongodb+srv://cluster0.eldg9.mongodb.net/<dbname>" --username pacy1'
+mongoose.connect(mongoUrl,  {
+    useNewUrlParser : true,
+    useUnifiedTopology : true
+})
+
+mongoose.connection.on('connected', () => {
+    console.log('connected successfully')
+})
+
+mongoose.connection.on('error', (err) => {
+    console.log('connection failled with error '+err)
+})
+
 app.get('/', (res, req) => {
     res.setEncoding('welcome')
 })
