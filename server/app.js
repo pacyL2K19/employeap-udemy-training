acf59017cd3a866f186149a100155d27ae382a49
Pacifique Linjanja <pacilinja1@icloud.com>
2020-07-06 01:28:29 +0200

    Mongo url and configuration

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 const app = express ()
-
+const mongoUrl = 'mongo "mongodb+srv://cluster0.eldg9.mongodb.net/<dbname>" --username pacy1'
 app.get('/', (res, req) => {
     res.setEncoding('welcome')
 })
