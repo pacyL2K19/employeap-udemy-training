53a2aacf51156e3b6f5c493d90102f7f0636d740
EXPRESSION <expression@MacBook-Pro-de-QS-2.local>
2020-12-05 17:03:42 +0100

    Solve an issue

--- screens/Home.js
+++ screens/Home.js
@@ -57,7 +57,7 @@ const Home = ({navigation}) => {
                 renderItem = {({item}) => {
                     return renderList(item)
                 }}
-                onRefresh = {}
+                // onRefresh = {}
                 refreshing = {loading}
             />
             <FAB
